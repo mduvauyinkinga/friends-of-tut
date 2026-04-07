9618545578a13f7046b2b9bff006ba264d07d8fb
mduvauyinkinga <224753020@tut4life.ac.za>
2026-04-08 00:43:46 +0200

    Delete src directory

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-// PWA disabled - workbox bug
-// import { registerSW } from "virtual:pwa-register";
-// registerSW({ immediate: true });
-
-createRoot(document.getElementById("root")!).render(<App />);
